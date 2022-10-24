40f055c04dfe116f7fd10e025a86751ce2ca434a
Z-CODES <zcodes.dev@gmail.com>
2022-10-24 15:16:50 +0100

    Updated Scroll to top

--- components/ScrollToTop.jsx
+++ components/ScrollToTop.jsx
@@ -4,37 +4,42 @@ import { useEffect, useState } from "react";
 
 const ScrollToTop = () => {
   const [visible, setVisible] = useState(false);
+  
+  useEffect(() => {
+  const toggleVisible = () => {
+    if (window.pageYOffset > 200) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  };
+ }, []);   
 
   useEffect(() => {
-    const toggleVisible = () => {
-      const scrolled = document.documentElement.scrollToTop;
-
-      if (scrolled > 100) {
-        setVisible(true);
-      } else if (scrolled <= 300) {
-        setVisible(false);
-      }
-    };
+    const scrollToTop = 
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    
     window.addEventListener("scroll", toggleVisible);
+
     return () => {
       window.removeEventListener("scroll", toggleVisible);
     };
   }, []);
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-
   return (
-    <button
-      className={styles.button}
-      style={{ display: visible ? "inline" : "none" }}
-    >
-      <span style={{ transform: "rotate(90deg)" }}>&gt;</span>
-    </button>
+    <div className={styles.container}>
+      <button
+        type="button"
+        onClick={scrollToTop}
+        className={styles.button}
+        style={{ display: visible ? "inline-flex" : "none" }}
+      >
+        ^
+      </button>
+    </div>
   );
 };
 
